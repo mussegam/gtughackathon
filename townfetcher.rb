a68bfdbd7237e225bdde05eaa09c3f83ef822945
Javi Dolcet <austorm@gmail.com>
2011-02-12 12:51:14 +0100

    we have the url of the wikipedia's page for every town in catalonia

--- townfetcher.rb
+++ townfetcher.rb
@@ -2,6 +2,7 @@
 
 require 'rubygems'
 require 'mechanize'
+require 'open-uri'
 
 class Town
 
@@ -14,3 +15,25 @@ class Town
 	end
 
 end
+
+def fetch_towns_of_catalonia
+  a = Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari' # Wikipedia blocks if not
+  }
+  
+  base_url = 'http://ca.wikipedia.org'
+    
+  a.get('http://ca.wikipedia.org/wiki/Llista_de_municipis_de_Catalunya') do |page|
+    table = page.search("//table[@class='wikitable sortable']").first;
+    table.search('tr').each do |row|
+      cell = row.search('td').first
+      if (!cell.nil?) then
+        node = cell.search('a').first
+        url = base_url + node['href']
+        puts url
+      end
+    end
+  end
+end
+
+fetch_towns_of_catalonia
